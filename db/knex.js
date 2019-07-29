8788d5a6ee4334281525273cace3cecfdfd38cf9
BlaineAndersonDev <BlaineAndersonDev@Gmail.com>
2019-07-29 00:29:26 -0700

    Added Postgresql database to heroku. Added function in knex for startup.

--- db/knex.js
+++ db/knex.js
@@ -12,21 +12,23 @@ const knex = require('knex')(config[process.env.NODE_ENV] || config['development
 
 // Uncomment & use when initially getting knex working on heroku.
 // DO NOT USE IN REAL PRODUCTION EVER!!!
-      // const knexSetup = async () => {
-      //   // Setup Knex if run in Heroku.
-      //   if (process.env.NODE_ENV != 'development') {
-      //     console.log('>>> Production Environment Detected: Running `knex migrate:rollback`, `knex migrate:latest` & `knex seed:run`.')
-      //     await knex.migrate.rollback('production')
-      //     console.log('>>> Rollback Complete.')
-      //     await knex.migrate.latest('production')
-      //     console.log('>>> Migrations Complete.')
-      //     await knex.seed.run('production')
-      //     console.log('>>> Seed Complete.')
-      //   }
-      // }
+      const knexSetup = async () => {
+        // Setup Knex if run in Heroku.
+        if (process.env.NODE_ENV != 'development') {
+          console.log('>>> Production Environment Detected: Running `knex migrate:rollback`, `knex migrate:latest` & `knex seed:run`.')
+          await knex.migrate.rollback('production')
+          console.log('>>> Rollback Complete.')
+          await knex.migrate.latest('production')
+          console.log('>>> Migrations Complete.')
+          await knex.seed.run('production')
+          console.log('>>> Seed Complete.')
+        }
+      }
 
 // This log just let's us know Knex started properly.
 // const knexSetup = async () => { console.log('Knex Database Connected...') };
-// knexSetup();
+
+
+knexSetup();
 
 module.exports = knex;
